9d4e8c951612176cf4394ac2b244274b017793ea
apple <apple@appledemacbook-pro.local>
2017-11-25 11:49:00 +0800

    修改换肤存在的小bug

--- 换肤/TwoViewController.m
+++ 换肤/TwoViewController.m
@@ -16,9 +16,14 @@
 
 @implementation TwoViewController
 
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+     self.faceImageView.image = [SkinTool skinToolWithImageName:@"face"];
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.faceImageView.image = [SkinTool skinToolWithImageName:@"face"];
+    //不要把换肤的代码放到viewDidLoad里面，因为控制器是懒加载的，一旦加载上就不会重新加载，所以viewDidLoad只会走一次，在切换了皮肤，后面的页面并不会变化所以要放在viewWillAppear，因为每次都会加载，假如这个控制器是push进来的那放在viewDidLoad也可以实现换肤效果，因为push进来的控制器在pop回去的时候回自动销毁，这样每次进来都是重新加载的，都会走viewDidLoad
+    //self.faceImageView.image = [SkinTool skinToolWithImageName:@"face"];
 }
 
 - (void)didReceiveMemoryWarning {
